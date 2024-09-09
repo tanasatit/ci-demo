026332df4a7912d689db2032e6b4f784cd89b0a7
tanasatit <tanasatit.n@ku.th>
2024-09-09 14:53:25 +0700

    add docstrings

--- statistics.py
+++ statistics.py
@@ -1,15 +1,24 @@
+"""
+A module that provides statistical functions like average, variance, and standard deviation.
+"""
+
 from math import sqrt
 
 
 def average(data):
-    """Return the average of a list of numeric values in data."""
+    """Calculate and return the average of a list of numeric values in data.
+
+    :param data: List of numeric values.
+    :returns: The average of the values in data.
+    :raises ValueError: If the list is empty.
+    """
     if len(data) == 0:
         raise ValueError("List must contain at least one value")
     return sum(data) / len(data)
 
 
 def variance(data):
-    """the population variance of a list of numbers in data.
+    """Calculate the population variance of a list of numbers in data.
 
     The variance is the sum of squared differences between data values
     and their mean, divided by the number of items in the list.
@@ -18,10 +27,9 @@ def variance(data):
 
     Example: variance([1,5]) is ((1-3)**2 + (5-3)**2)/2 = 4.
 
-    :param data: list of numbers for which variance will be computed.
-        Must contain at least one element.
-    :returns: population variance of values in data list.
-    :raises ValueError: if the data parameter is empty.
+    :param data: List of numbers for which variance will be computed.
+    :returns: Population variance of values in the data list.
+    :raises ValueError: If the list is empty.
 
     >>> variance([1])
     0.0
@@ -32,14 +40,18 @@ def variance(data):
     >>> variance([1000000, 1000004])
     4.0
     """
-    # ugly code.
     n = len(data)
     if n == 0:
         raise ValueError("List must contain at least one value")
     avg = average(data)
-    return sum([(x - avg) ** 2 for x in data]) / n
+    return sum((x - avg) ** 2 for x in data) / n
 
 
 def stdev(data):
-    """the standard deviation of a list of values"""
+    """Calculate the standard deviation of a list of values.
+
+    :param data: List of numbers for which standard deviation will be computed.
+    :returns: Standard deviation of values in the data list.
+    :raises ValueError: If the list is empty.
+    """
     return sqrt(variance(data))
