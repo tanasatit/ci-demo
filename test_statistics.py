1dbcac64fa8d5528298cc545d5d37cafbd06a61d
tanasatit <tanasatit.n@ku.th>
2024-09-09 14:29:30 +0700

    fix error

--- test_statistics.py
+++ test_statistics.py
@@ -2,20 +2,21 @@ from unittest import TestCase
 from statistics import variance, stdev
 from math import sqrt
 
+
 class StatisticsTest(TestCase):
 
     def test_variance_typical_values(self):
         """variance of typical values"""
-        self.assertEqual(0.0, variance([10.0,10.0,10.0,10.0,10.0]))
-        self.assertEqual(2.0, variance([1,2,3,4,5]))
-        self.assertEqual(8.0, variance([10,2,8,4,6]))
-    
+        self.assertEqual(0.0, variance([10.0, 10.0, 10.0, 10.0, 10.0]))
+        self.assertEqual(2.0, variance([1, 2, 3, 4, 5]))
+        self.assertEqual(8.0, variance([10, 2, 8, 4, 6]))
+
     def test_variance_non_integers(self):
         """variance should work with decimal values"""
         # variance([x,y,z]) == variance([x+d,y+d,z+d]) for any d
-        self.assertEqual(4.0, variance([0.1, 4.1]))
+        self.assertAlmostEqual(4.0, variance([0.1, 4.1]))
         # variance([0,4,4,8]) == 8
-        self.assertEqual(8.0, variance([0.1, 4.1, 4.1, 8.1]))
+        self.assertAlmostEqual(8.0, variance([0.1, 4.1, 4.1, 8.1]))
 
     def test_stdev(self):
         # standard deviation of a single value should be zero
@@ -28,5 +29,5 @@ class StatisticsTest(TestCase):
 
 if __name__ == '__main__':
     import unittest
-    unittest.main(verbosity=1)
 
+    unittest.main(verbosity=1)
